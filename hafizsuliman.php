9bf3c6275cbb29a022c45a08525e9ab7714c60cf
Hafizsuliman <74912623+Hafizsuliman@users.noreply.github.com>
2020-11-23 12:36:26 -0800

    Add files via upload

---
+++ hafizsuliman.php
@@ -0,0 +1,17 @@
+﻿<html>
+<head>
+<title>first page</title>
+</head>
+<body bgcolor="ff4433">
+
+<h1>this is my second bage</h1><br>
+<a href="first.html"click here go to the first bage</a>
+
+
+
+<h5 aling="center">بسم الله الله الرحمن الرحيم</h5>
+<h5 aling="leaft">كليه الامارات للعلوم والتكنولوجيا</h5>
+<h5 aling="right">بكالاريوس تقانه المعلومات</h5>
+
+</body>
+</html>
